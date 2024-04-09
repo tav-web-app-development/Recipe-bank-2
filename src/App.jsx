7ecf1db667247955b098eb8693d79d7ad1359551
Shalom Steinmetz <shalomsteinmetz@gmail.com>
2024-04-09 16:10:09 -0400

    fix readme

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,31 @@ import RecipeContainer from "./Components/RecipeContainer";
 import "./assets/style.css";
 
 function App() {
-  const [recipes, setRecipes] = useState({});
+  const [recipes, setRecipes] = useState([]);
   useEffect(() => {
     fetch("https://api.sampleapis.com/recipes/recipes")
       .then((res) => {
-        console.log("Unmounted");
         return res.json();
       })
       .then((data) => {
         setRecipes(data);
       });
+    return () => console.log("unmounted");
   }, []);
+  function filterRecipesComputeIntensive(recipes) {
+    const now = performance.now();
+    while (performance.now() - now < 8000) {
+      //spin()
+    }
+    return list.filter((word) => word.name.split(" ").length <= 4);
+  }
+  const filteredRecipes = filterRecipesComputeIntensive(recipes);
   return (
     <>
       <Navbar />
-      {recipes.length &&
-        recipes.map((data) => <RecipeContainer recipe={data} key={data.id} />)}
+      {recipes.map((data) => (
+        <RecipeContainer recipe={data} key={data.id} />
+      ))}
       <Footer />
     </>
   );
